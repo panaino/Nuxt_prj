ae2f80f4208633d645d3b501180071b9e5675e7d
panaino <panaino66576657@gmail.com>
2020-09-13 02:51:20 +0900

    テキストエリアのコンポーネント作成

---
+++ components/InputText.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <label :for="inputTextInfo.tagId">{{ inputTextInfo.text }}</label>
+        <input :id="inputTextInfo.tagId" type="text">
+    </div>
+</template>
+
+<script>
+export default {
+    props:['inputTextInfo']
+}
+</script>
--- components/RegisterModal.vue
+++ components/RegisterModal.vue
@@ -1,21 +1,29 @@
 <template>
 <div>
 	<div class="register_btn_area">
-		<DefultButton class="register_btn_show" btnText="登録" @btnClick="showModal" />
+		<DefultButton class="register_btn_show" btnText="登録" @btn-click="showModal" />
 	</div>
 	<div class="register_modal_full" v-show="visible">
 		<div class="register_modal_content">
 			<div>
-				<label>
-					性格
-					<select name="" id="">
-						<option hidden>選択してください</option>
-						<option value="" v-for="item in personalInfo" :key="item.id">{{ item.PESONRALITY }}</option>
-					</select>
+				<InputText :inputTextInfo="inputTextInfo.item1" />
+				<label for="pesonality">性格
 				</label>
+				<select id="pesonality">
+					<option hidden>選択してください</option>
+					<option value="" v-for="item in personalInfo" :key="item.id">{{ item.PESONRALITY }}</option>
+				</select>
+				<InputText :inputTextInfo="inputTextInfo.item2" />
+				<InputText :inputTextInfo="inputTextInfo.item3" />
+				<InputText :inputTextInfo="inputTextInfo.item4" />
+				<InputText :inputTextInfo="inputTextInfo.item5" />
+				<InputText :inputTextInfo="inputTextInfo.item6" />
+				<InputText :inputTextInfo="inputTextInfo.item7" />
+				<InputText :inputTextInfo="inputTextInfo.item8" />
 			</div>
 			<div class="register_btn_area">
-				<DefultButton class="register_btn_close" btnText="閉じる" @btnClick="closeModal" />
+				<DefultButton class="register_btn_close" btnText="登録" />
+				<DefultButton class="register_btn_close" btnText="閉じる" @btn-click="closeModal" />
 			</div>
 		</div>
 	</div>
@@ -27,7 +35,17 @@ export default {
 	props:['personalInfo'],
     data() {
 		return {
-			visible: false
+			visible: false,
+			inputTextInfo: {
+				item1:{"tagId":"name", "text":"名前"},
+				item2:{"tagId":"ability", "text":"特性"},
+				item3:{"tagId":"statusH", "text":"HP"},
+				item4:{"tagId":"statusA", "text":"攻撃"},
+				item5:{"tagId":"statusB", "text":"防御"},
+				item6:{"tagId":"statusC", "text":"特攻"},
+				item7:{"tagId":"statusD", "text":"特防"},
+				item8:{"tagId":"statusS", "text":"素早さ"}
+			}
 		}
 	},
 	methods: {
@@ -42,6 +60,9 @@ export default {
 </script>
 
 <style>
+label {
+	display: block;
+}
 /** モーダルに関するデザイン */
 .register_modal_full{
   /*　要素を重ねた時の順番　*/
@@ -72,12 +93,12 @@ export default {
 .register_btn_area {
 	display: flex;
 	justify-content: flex-end;
-	margin: 10px 10px 0 0;
 }
 /** ボタンに関するデザイン */
 .register_btn_show {
 	width: 100px;
 	padding: 0.8em;
+	margin: 10px 10px 0 0;
 }
 
 .register_btn_close {
