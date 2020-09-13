affa3deb160acb4db6403af439150ce084f2a4c1
panaino <panaino66576657@gmail.com>
2020-09-14 00:21:17 +0900

    メタ情報追加 日本語対応

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,6 +19,7 @@ export default {
   head: {
     title: process.env.npm_package_name || '',
     meta: [
+      { lang: 'ja' },
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
